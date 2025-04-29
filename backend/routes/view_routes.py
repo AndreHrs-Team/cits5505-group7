7176d06268d81c07d62d586fb5392329cb89ec65
Andre Harsono <andreharsono27@gmail.com>
2025-04-29 21:22:38 +0800

    Modify navigation bar and add navigation tabs template

--- backend/routes/view_routes.py
+++ backend/routes/view_routes.py
@@ -13,4 +13,16 @@ def init_view_routes(app):
 
     @app.route("/finance")
     def render_finance_page():
-        return render_template('finance/index.html', title="Fitness")
+        return render_template('finance/finance_transaction.html', title="Finance - Transaction")
+
+    @app.route("/finance/accounts")
+    def render_finance_accounts_page():
+        return render_template('finance/finance_accounts.html', title="Finance - Accounts")
+
+    @app.route("/finance/categories")
+    def render_finance_categories_page():
+        return render_template('finance/finance_categories.html', title="Finance - Categories")
+
+    @app.route("/finance/insight")
+    def render_finance_insight_page():
+        return render_template('finance/finance_insight.html', title="Finance - Insight")
