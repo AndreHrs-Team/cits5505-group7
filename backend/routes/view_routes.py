bf7d994092188de6cfe0efc0c3a2b3104db20ea6
Andre Harsono <andreharsono27@gmail.com>
2025-04-29 20:50:55 +0800

    Added navigation template

--- backend/routes/view_routes.py
+++ backend/routes/view_routes.py
@@ -1,17 +1,16 @@
 from flask import render_template
 
+
 def init_view_routes(app):
     @app.route("/")
     @app.route("/index")
     def render_index_page():
         return render_template('index.html', title="Home")
-    
-    # Add more view routes here
+
     @app.route("/fitness")
     def render_fitness_page():
         return render_template('fitness/index.html', title="Fitness")
-    
-    # Add more view routes here
+
     @app.route("/finance")
     def render_finance_page():
         return render_template('finance/index.html', title="Fitness")
