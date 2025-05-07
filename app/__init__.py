785a614aa9856065386f3898a5a89494e7f8ed03
Angelhuangggg <144416190+Angelhuangggg@users.noreply.github.com>
2025-05-07 22:40:32 +0800

    updata2

--- app/__init__.py
+++ app/__init__.py
@@ -93,19 +93,15 @@ def create_app(config_name=None):
         with app.app_context():
             from app.models import User
             app.logger.info("Creating database tables if they don't exist...")
-<<<<<<< HEAD
-            db.create_all()
-            app.logger.info("Database tables created successfully.")
 
-            # Create admin user if not exists
-=======
+
             # Auto create database
             db.create_all()
             app.logger.info("Database tables created successfully.")
 
             ## Create admin user if not exists
             ## Breaking if using migration without having any tables
->>>>>>> dc77408547da44c66bf0a20e5ea8c9345dd3a276
+
             admin = User.query.filter_by(username='admin').first()
             if not admin:
                 app.logger.info("Creating admin user...")
